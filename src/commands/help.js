9a763cba9cc8bee43be7a26774d48b08a07ebcd4
Yasmin Carvalho <miojodepopoca@gmail.com>
2021-06-17 09:11:52 -0300

    Embed adicionada a função Help

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,14 +1,21 @@
+const embed = require('discord.js');
 const execute = (client,msg,args) =>
 {
     let string = "==== COMANDOS: ====\n";
+
+    var msghelp = new embed.MessageEmbed()
+    .setColor('#e1a700')
+    .setTitle('░░░░░░░░░░░░░  COMANDOS:  ░░░░░░░░░░░░░')
+
     client.commands.forEach((command) => {
         if(command.help)
         {
+            msghelp.addFields({name :`Comando:  ${command.name} `, value:`Função:  ${command.help}`, inline:true});
             string += `**${command.name}**: ${command.help} \n`
         }
     });
 
-    return msg.channel.send(string);
+    return msg.channel.send(msghelp);
 };
 
 module.exports = {
