15eb34be8a3f6da556592bcef549787f4053bdc9
Yasmin Carvalho <miojodepopoca@gmail.com>
2021-06-16 22:02:39 -0300

    voltas

--- src/index.js
+++ src/index.js
@@ -1,55 +1,55 @@
-var http = require('http');
+
 const discord = require('discord.js');
 const dotenv = require('dotenv');
 const fs = require('fs');
 const path = require('path');
 const { OpusEncoder } = require('@discordjs/opus');
 
+dotenv.config();
 
-http.createServer(function (request, response) {
-    dotenv.config();
+const client = new discord.Client();
+client.commands = new discord.Collection();
+client.login(process.env.TOKEN);
 
-    const client = new discord.Client();
-    client.commands = new discord.Collection();
-    client.login(process.env.TOKEN);
-    
-    const commandFiles = fs.readdirSync(path.join(__dirname, "/commands")).filter(filename => filename.endsWith(".js"));
-    
-    
-    client.queues = new Map();
-    
-    //ADICIONA OS COMANDOS
-    for (var filename of commandFiles) {
-        const command = require(`./commands/${filename}`);
-        client.commands.set(command.name, command);
-    }
-    
-    //MAPEA OS COMANDOS
-    
-    console.log(client.commands); 
-    client.on("ready", function () {
-
-        console.log(`ESTOU CONECTADO by ${client.user.username}`);
-    });
-
-    client.on("message", (msg) => {
-
-        // if(!msg.content.startWith(process.env.PREFIX)) return;
-        if (msg.author.bot) return;
-        if (msg.channel.type === "dm") return msg.reply("Desculpe, não respondo a mensagens privadas!\nEntretanto entre em algum dos servidores no qual eu participo.\n");
-        if (!msg.content.startsWith(process.env.PREFIX)) return;
+const commandFiles = fs.readdirSync(path.join(__dirname,"/commands")).filter(filename=> filename.endsWith(".js"));
 
 
-        const args = msg.content.slice(process.env.PREFIX.length).split(" ");
-        const command = args.shift();
+client.queues = new Map();
 
-        try {
+//ADICIONA OS COMANDOS
+for(var filename of commandFiles)
+{
+    const command = require(`./commands/${filename}`);
+    client.commands.set(command.name,command);
+}
 
-            client.commands.get(command).execute(client, msg, args);
+//MAPEA OS COMANDOS
 
-        } catch (e) {
-            return msg.reply("Ops! Eu ainda não conheço esse comando!");
-        }
+console.log(client.commands);
 
-    });
-}).listen(process.env.PORT || 5000);
+client.on("ready", function(){
+
+    console.log(`ESTOU CONECTADO by ${client.user.username}`);
+});
+
+client.on("message", (msg)=>{
+
+   // if(!msg.content.startWith(process.env.PREFIX)) return;
+   if(msg.author.bot) return;
+   if (msg.channel.type === "dm") return msg.reply("Desculpe, não respondo a mensagens privadas!\nEntretanto entre em algum dos servidores no qual eu participo.\n");
+   if(!msg.content.startsWith(process.env.PREFIX) ) return;
+   
+
+   const args = msg.content.slice(process.env.PREFIX.length).split(" ");
+   const command = args.shift();
+
+   try{
+
+        client.commands.get(command).execute(client,msg,args);
+
+   }catch(e)
+   {
+        return msg.reply("Ops! Eu ainda não conheço esse comando!");
+   }
+
+});
